38a4222fbd535422383374e3103848d3b2a74eff
lFreitasc <lucas.freitascosta96@gmai.com>
2019-04-30 20:26:50 -0300

    dependencias e classe do banco

--- serverConfig.js
+++ serverConfig.js
@@ -2,19 +2,27 @@ let express = require('express')
     app = express()
     path = require('path')
     bodyParser = require('body-parser')
-
+    cookieParser = require('cookie-parser')
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine', 'hbs')
 app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.get('/', (req, res) => {
+app.post('/login', (req, res) => {
+    res.cookie('login', req.body.login)
+    res.redirect('/')
+})
 
-    res.render('index')
+app.get('/', (req, res) => {
+    if(req.cookies && req.cookies.login){
+        res.render('index', {user:req.cookies.login})
+    }
+    res.render('login')
 })
 
 app.listen(8000, ()=> {
